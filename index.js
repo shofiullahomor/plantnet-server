86a5f6158d66bb6a8c8f3b0cdfaa2dc571ecba4f
shofiullah omor <shofiullahomor1436@gmail.com>
2025-04-14 09:13:30 +0600

    role api added

--- index.js
+++ index.js
@@ -48,6 +48,69 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    const db = client.db("plantNet-session");
+    const usersCollection = db.collection("users");
+    const plantsCollection = db.collection("plants");
+    const ordersCollection = db.collection("orders");
+    // users data saveing in db
+    app.post("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+      //  check if user already exist in db
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist);
+      }
+      const result = await usersCollection.insertOne({
+        ...user,
+        role: "customer",
+        timestamp: Date.now(),
+      });
+      res.send(result);
+    });
+    // manage user status
+    app.patch("/users/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      // const status = req.body.status;
+      const filter = { email };
+      const user = await usersCollection.findOne(filter);
+      if (!user || user?.status === "Requested")
+        return res
+          .status(400)
+          .send("You have already requested to become a seller");
+      const updateDoc = {
+        $set: {
+          status: "Requested",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // get all users
+    app.get("/all-users/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: { $ne: email } };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+    // update a user role & status
+    app.patch("/user/role/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const { role } = req.body;
+      const filter = { email };
+      const updateDoc = {
+        $set: { role, status: "Approved" },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // get user role
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send({ role: result?.role });
+    });
     // Generate jwt token
     app.post("/jwt", async (req, res) => {
       const email = req.body;
@@ -75,6 +138,102 @@ async function run() {
       } catch (err) {
         res.status(500).send(err);
       }
+    });
+    // save a plant in db
+    app.post("/plants", verifyToken, async (req, res) => {
+      const plant = req.body;
+      const result = await plantsCollection.insertOne(plant);
+      res.send(result);
+    });
+    // get all plants from db
+    app.get("/plants", async (req, res) => {
+      const result = await plantsCollection.find().toArray();
+      res.send(result);
+    });
+    // get a plant by id
+    app.get("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantsCollection.findOne(query);
+      res.send(result);
+    });
+    // save order data in db
+    app.post("/order", verifyToken, async (req, res) => {
+      const orderInfo = req.body;
+      console.log(orderInfo);
+      const result = await ordersCollection.insertOne(orderInfo);
+      res.send(result);
+    });
+    // Manage plant quantitiy
+    app.patch("/plants/quantity/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const { quantityToUpdate, status } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      let updateDoc = {
+        $inc: { quantity: -quantityToUpdate },
+      };
+      if (status === "increase") {
+        updateDoc = {
+          $inc: { quantity: quantityToUpdate },
+        };
+      }
+      const result = await plantsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // get all orders for a specific customer
+    app.get("/customer-orders/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { "customer.email": email };
+      const result = await ordersCollection
+        .aggregate([
+          {
+            $match: query, // Match specific customers data only by email
+          },
+          {
+            $addFields: {
+              plantId: { $toObjectId: "$plantId" }, // convert plant string field to object field
+            },
+          },
+          {
+            // go to a different collection and look for data
+            $lookup: {
+              from: "plants", // collection name
+              localField: "plantId", // local data that you want to match
+              foreignField: "_id", // foreign field name of that same data
+              as: "plants", // return the data as plants array (array naming)
+            },
+          },
+          {
+            $unwind: "$plants", // unwind lookup result , return without array
+          },
+          {
+            $addFields: {
+              //
+              name: "$plants.name",
+              image: "$plants.image",
+              category: "$plants.category",
+            },
+          },
+          {
+            $project: {
+              plants: 0,
+            },
+          },
+        ])
+        .toArray();
+      res.send(result);
+    });
+    // cancel/delete an order
+    app.delete("/orders/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      if (order.status === "Delivered")
+        return res
+          .status(409)
+          .send("Can not cancel once the product is delivered");
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
